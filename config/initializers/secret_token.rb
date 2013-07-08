c2ef4fc72860d1960a19a18a59a46c6d814aad89
Patrickmnts <patrickmnts@gmail.com>
2013-07-08 11:29:58 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsPlay::Application.config.secret_key_base = '80def2be02fe62711176927909e5b206fd0e9905b8707555fa2443e40a9820d685e4a8a8e2ccae987e66eee24191ad71dfa4dddd91b080306d5b5b52fefe9b33'
